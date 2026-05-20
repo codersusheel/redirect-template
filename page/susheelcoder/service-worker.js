e57f38daef5a9f18dae84290b7e84eb7d62ea195
susheel kumar <techiesusheel@gmail.com>
2026-05-20 15:39:16 +0530

    ,

--- page/susheelcoder/service-worker.js
+++ page/susheelcoder/service-worker.js
@@ -1,15 +1,15 @@
-const CACHE = "index-cache";
+// const CACHE = "index-cache";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.add("index.html"))
-  );
-});
+// self.addEventListener("install", e => {
+//   e.waitUntil(
+//     caches.open(CACHE).then(c => c.add("index.html"))
+//   );
+// });
 
-self.addEventListener("fetch", e => {
-  if (e.request.mode === "navigate") {
-    e.respondWith(
-      fetch(e.request).catch(() => caches.match("index.html"))
-    );
-  }
-});
+// self.addEventListener("fetch", e => {
+//   if (e.request.mode === "navigate") {
+//     e.respondWith(
+//       fetch(e.request).catch(() => caches.match("index.html"))
+//     );
+//   }
+// });
